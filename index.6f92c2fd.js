c84cc5ae3f4c9e010542151e877a85f0b70ef1b4
vova <hohiop09@gmail.com>
2024-06-29 12:29:50 +0300

    make build

---
+++ index.6f92c2fd.js
@@ -0,0 +1,3 @@
+var e=document.querySelector(".burger__logo"),t=document.querySelector("body"),o=document.querySelectorAll(".burger__nav-LIst-Item");document.getElementById("contactForm").addEventListener("submit",function(e){e.preventDefault()}),e.addEventListener("click",function(){console.log(1),e.classList.toggle("active"),t.classList.toggle("overflow-hidden")}),o.forEach(function(o){return o.addEventListener("click",function(){e.classList.remove("active"),t.classList.remove("overflow-hidden")})});//# sourceMappingURL=index.6f92c2fd.js.map
+
+//# sourceMappingURL=index.6f92c2fd.js.map
